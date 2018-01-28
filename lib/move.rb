b2b67f40d2bd6126f52a8edd021fe81d6defb5ef
Brittany Bellantoni <brittt2121@hotmail.com>
2018-01-28 23:50:37 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,7 +7,8 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  puts "1, 2, 3, 4, 5, 6, 7, 8, 9"
+  puts = input.to_i - 1
 end
 
-def move(board, input)
+def move(board, index, character = "X")
+  
